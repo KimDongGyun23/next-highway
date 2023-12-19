45be08e8d61a8a4a1ff879f510131d04b00ca95f
김동균 <wooheuy5752@nate.com>
2023-12-19 16:44:03 +0900

    Feat(Amenities) : 세부 페이지 틀 완성
    세부 페이지에 사용될 틀 완성.
    각각의 페이지에 맞게 api 요청과 필터링 기능 추가 필요.
    pagination 기능 추가 필요.

--- src/app/(amenities)/amenities/AmenitiesClient.jsx
+++ src/app/(amenities)/amenities/AmenitiesClient.jsx
@@ -1,39 +1,81 @@
 'use client'
-import axios from 'axios';
-import React from 'react'
+import axios, { all } from 'axios';
+import React, { useEffect, useState } from 'react'
 import styles from './AmenitiesClient.module.scss'
 import Sidebar from '@/layout/sidebar/Sidebar';
+import { IoSearchSharp } from "react-icons/io5";
 
 const AmenitiesClient = () => {
+  const [allHighwayInfo, setAllHighwayInfo] = useState([]);
+  const [displayedHighwayInfo, setDisplayedHighwayInfo] = useState([]);
 
-  const handleClick = async ()=>{
+  const limitNum = 7;
 
-    // 총 고속도로 수 = count 변수에 저장됨
-    // const res = await axios.get("https://data.ex.co.kr/openapi/restinfo/restBrandList?key=test&type=json&numOfRows=10&pageNo=1");
-    const res = await axios.get("https://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList?key=test&type=json");
-    console.log(res.data);
+
+  // 휴게소 정보 저장
+  const getHighwayInfo = async () => {
+    try {
+
+      // svarGsstClassCd = 0:휴게소 1:주유소 
+      const res = await axios.get("https://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList?key=test&type=json&svarGsstClssCd=0");
+      setAllHighwayInfo(res.data.list);
+      setDisplayedHighwayInfo(filterDisplayedHighwayInfo(res.data.list))
+    } 
+    catch (error) {
+      console.log(error);
+    }
   }
 
-  return (
+  // limitNum 만큼 displayedHighwayInfo에 추가
+  function filterDisplayedHighwayInfo(allInfo, displayedHighwayInfo = []){
+    const limit = displayedHighwayInfo.length + limitNum;
+
+    // limitNum 만큼 더 가져오기
+    const array = allInfo.filter((_, index)=> index+1 <= limit)
     
-    // <button onClick={handleClick}>클릭</button>
+    return array;
+  }
+  
+  console.log(displayedHighwayInfo);
+
+  useEffect(() => {
+    getHighwayInfo();
+  }, [])
 
 
+  return (
     <div className={styles.container}>
 
       <div>
         <Sidebar />
       </div>
       
-      <div>
+      <div className={styles.content}>
         <form>
-          <input />
-          <button type='submit'></button>
+          <input 
+            type='text'
+          />
+          <button type='submit'><IoSearchSharp /></button>
         </form>
 
-        <div>
-          list
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>휴게소 명</th>
+              <th>주소</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              displayedHighwayInfo.map(({svarCd, svarNm, svarAddr})=>(
+                <tr className={styles.trBody} key={svarCd}>
+                  <td>{svarNm}</td>
+                  <td className={styles.addr}>{svarAddr}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
 
     </div>
