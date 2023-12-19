591237c12e12000bed18d16fab63539786b761f6
김동균 <wooheuy5752@nate.com>
2023-12-19 14:30:08 +0900

    Refactor(Sidebar) : sidebar 수정
    클릭 시, 디자인 변경 완료

--- src/layout/sidebar/SidebarClient.jsx
+++ src/layout/sidebar/SidebarClient.jsx
@@ -1,86 +1,30 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SidebarClient.module.scss'
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleMenu } from '@/store/slice/menuSlice';
-import Link from 'next/link'
+
+
+const AreaName = [
+  "서울", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남"
+];
 
 const SidebarClient = () => {
-  const dispatch = useDispatch();
-
-  const { isActive } = useSelector((state)=>state.menu);
-
-
-
+  const [activeCity, setActiveCity] = useState("");
+  console.log(activeCity)
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>지역 선택</h3>
 
       <ul>
-        <li
-          
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          <Link 
-            href={'/'}
-            className={({ isActive }) => isActive ? "styles.active" : ""}>
-            서울
-          </Link>
-        </li>
-        {/* <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          서울
-        </li> */}
-        {/* <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          경기
-        </li>
-        <li
-          className={active ? styles.active : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          강원
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          충북
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          충남
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          전북
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          전남
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          경북
-        </li>
-        <li
-          className={active ? 'styles.active' : ""}
-          onClick={() => dispatch(toggleMenu(false))}
-        >
-          경남
-        </li> */}
+        {
+          AreaName.map((area)=>(
+            <li key={area}
+                className={activeCity === area ? styles.active : null}
+                onClick={()=>setActiveCity(area)}
+              >
+                {area}
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
