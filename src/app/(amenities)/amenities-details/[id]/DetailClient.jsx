ce10166e23c35a6cf2f638b13356955d16987f32
김동균 <wooheuy5752@nate.com>
2024-01-11 21:24:00 +0900

    Feat (DetailClient) : 게시된 정보 없을 시 디자인
    졸음 쉼터같은 게시된 정보가 없는 경우, 문구 출력

--- src/app/(amenities)/amenities-details/[id]/DetailClient.jsx
+++ src/app/(amenities)/amenities-details/[id]/DetailClient.jsx
@@ -1,24 +1,22 @@
 'use client'
 import axios from 'axios'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import styles from './DetailClient.module.scss'
+import Button from '@/components/button/Button'
 
 const DetailClient = () => {
   const [info, setInfo] = useState([]);
 
   const { id } = useParams();
+  const router = useRouter();
 
-  // const url = `https://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList?key=test&type=json&svarCd=${id}`
-
-
-  const url2 = `https://data.ex.co.kr/openapi/restinfo/restConvList?key=test&type=json&numOfRows=10&pageNo=1&stdRestCd=${id}`
+  const url = `https://data.ex.co.kr/openapi/restinfo/restConvList?key=test&type=json&numOfRows=10&pageNo=1&stdRestCd=${id}`
 
   const getInfo = async () => {
-    // const res = await axios.get(url);
-    const res2 = await axios.get(url2);
-    console.log(res2.data.list);
-    setInfo(res2.data.list);
+    const res = await axios.get(url);
+    console.log(res.data.list);
+    setInfo(res.data.list);
 
   }
 
@@ -29,28 +27,46 @@ const DetailClient = () => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles[`area-name`]}>
-        { info[0]?.stdRestNm }
-      </h2>
+      {
+        info.length === 0 ?
+        (
+          <div className={styles[`no-data`]}>
+            <p>게시된 정보가 없습니다.</p>
+            
+            <Button
+              onClick={()=>{router.back();}}
+            >
+              이전 페이지로 돌아가기
+            </Button>
+          </div>
+        ) :
+        (
+          <>
+            <h2 className={styles[`area-name`]}>
+              { info[0]?.stdRestNm }
+            </h2>
 
-      <p className={styles.addr}>
-        { info[0]?.svarAddr }
-      </p>
+            <p className={styles.addr}>
+              { info[0]?.svarAddr }
+            </p>
 
-      <div className={styles[`box-wrapper`]}>
-        {
-          info.map(({
-            psCode, psDesc, psName,
-            stime, etime,
-          })=>(
-            <div className={styles.box} key={psCode}>
-              <p className={styles.name}>{psName} ({psCode}) </p>
-              <p className={styles.desc}>{psDesc}</p>
-              <p className={styles.time}>이용시간 : {stime} - {etime}</p>
+            <div className={styles[`box-wrapper`]}>
+              {
+                info.map(({
+                  psCode, psDesc, psName,
+                  stime, etime,
+                })=>(
+                  <div className={styles.box} key={psCode}>
+                    <p className={styles.name}>{psName} ({psCode}) </p>
+                    <p className={styles.desc}>{psDesc}</p>
+                    <p className={styles.time}>이용시간 : {stime} - {etime}</p>
+                  </div>
+                ))
+              }
             </div>
-          ))
-        }
-      </div>
+          </>
+        )
+      }
     </div>
   )
 }
